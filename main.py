8c9323f0534ed8a9bacd966b3da18ced501becae
Mohamed Ahmed <moha2014394@gmail.com>
2017-03-10 23:53:42 +0200

    Canny Edge Detection

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from os import listdir, makedirs
 from os.path import isfile, join, exists
 import cv2
 import numpy as np
-
+  
 train_images_path = '../train'
 test_images_path = '../test'
 
@@ -18,19 +18,31 @@ for f in listdir(train_images_path):
     # check if it's a file
     if isfile(filename):
         print filename
+      
         # read the image file
         img = cv2.imread(filename, 0)
+
         # apply gaussian blur to remove random dots
         blurred_img = cv2.GaussianBlur(img,(3,3),0)
         # THRESH_OTSU determines the best threshold value based on the image histogram then uses this threshold
         ret, threshold_img = cv2.threshold(blurred_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
         # print the used threshold value (if pixel < ret: pixel is set to 0 else: 255)
         inverted_img = cv2.bitwise_not(threshold_img)
-        print ret
+        # Apply Canny edge detection
+        canny = cv2.Canny(img, 50, 240)
+        #Adding Canny and Threshold value
+        adding = cv2.add(canny,inverted_img)
+        #print the width and the lenght
+       # print inverted_img.shape
+
+        #crop the picture 
+       # cropped = inverted_img[720:1450, 100:1240]
+     
 
         # save the images
         result_filename = join(train_result_path, f)
-        cv2.imwrite(result_filename, inverted_img)
+        cv2.imwrite(result_filename, adding)
 
         # display images
         # cv2.namedWindow('image', cv2.WINDOW_FREERATIO)
