d402627df6b4c40136176329fbcc2e50a8b0d0b8
Eslam Samir <eslamsamir232@gmail.com>
2017-04-14 20:21:13 +0200

    some samples are correct

--- main.py
+++ main.py
@@ -2,15 +2,41 @@ from os import listdir, makedirs
 from os.path import isfile, join, exists
 import imutils
 import cv2
-import numpy as np
 from imutils.perspective import four_point_transform
 import matplotlib.pyplot as plt
+
+
+def question_number(y_value, column):
+    row = 1
+    while y_value > 30:
+        y_value -= 40
+        row += 1
+    return row + (15 * column)
+
+
+def answer(x_value):
+    if x_value < 50:
+        return 'A'
+    elif x_value < 90:
+        return 'B'
+    elif x_value < 130:
+        return 'C'
+    else:
+        return 'D'
+
+
+correct_answers = {1:  'B', 2:  'C', 3:  'A', 4:  'A', 5:  'D', 6:  'A', 7:  'C', 8:  'C', 9:  'A', 10: 'C', 11: 'A',
+                   12: 'B', 13: 'C', 14: 'C', 15: 'B', 16: 'A', 17: 'D', 18: 'B', 19: 'C', 20: 'B', 21: 'D', 22: 'C',
+                   23: 'D', 24: 'B', 25: 'D', 26: 'C', 27: 'D', 28: 'D', 29: 'B', 30: 'C', 31: 'B', 32: 'B', 33: 'D',
+                   34: 'C', 35: 'B', 36: 'C', 37: 'B', 38: 'C', 39: 'C', 40: 'A', 41: 'B', 42: 'B', 43: 'C', 44: 'C',
+                   45: 'B'}
+
 
 train_images_path = '../train'
 test_images_path = '../test'
 
 train_result_path = '../train_result'
-cropped_path='../cropped'
+cropped_path = '../cropped'
 
 if not exists(train_result_path):
     makedirs(train_result_path)
@@ -29,8 +55,8 @@ for f in listdir(train_images_path):
         # apply gaussian blur to remove random dots
 
         # THRESH_OTSU determines the best threshold value based on the image histogram then uses this threshold
-        #ret, threshold_img = cv2.threshold(blurred_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # ret, threshold_img = cv2.threshold(blurred_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(img, (5, 5), 0)
         edged = cv2.Canny(blurred, 130, 300)
 
@@ -39,15 +65,15 @@ for f in listdir(train_images_path):
 
         # ensure that at least one contour was found
         if len(cnts) > 0:
-        # sort the contours according to their size in
-        # descending order
+            # sort the contours according to their size in
+            # descending order
             cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
             rect = None
             # loop over the sorted contours
             for c in cnts:
-            # approximate the contour
+                # approximate the contour
                 (x, y, w, h) = cv2.boundingRect(c)
-                #print (x, y, w, h)
+                # print (x, y, w, h)
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
                 # if our approximated contour has four points,
@@ -69,32 +95,36 @@ for f in listdir(train_images_path):
                 kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
                 erosion = cv2.erode(closing, kernel2, iterations=1)
 
-                questions_cols = []
-                questions_cols.append(erosion[70:665, 90:270])
-                questions_cols.append(erosion[70:665, 418:598])
-                questions_cols.append(erosion[70:665, 745:925])
+                questions_cols = [erosion[70:665, 90:270],
+                                  erosion[70:665, 418:598],
+                                  erosion[70:665, 745:925]]
 
-                for questions_col in questions_cols:
-                    circles = cv2.HoughCircles(questions_col, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=10, minRadius=2, maxRadius=20)
+                answers = {}
+                score = 0
+                for i in range(len(questions_cols)):
+                    circles = cv2.HoughCircles(questions_cols[i], cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=10,
+                                               minRadius=2, maxRadius=20)
                     if circles is not None:
-                        for circle in circles[0]:
-                            print circle
-                            #cv2.circle(backtorgb, (circle[0], circle[1]), circle[2], (0, 0, 255), -1)
-                        fig, ax = plt.subplots()
-                        im = ax.imshow(backtorgb, interpolation='none')
-                        #plt.show()
-                        #cv2.imshow('image',backtorgb)
-                        # save the images
-                        result_filename = join(train_result_path, f)
-                        cv2.imwrite(result_filename, backtorgb)
-
-                        '''
-                        f1 = join(train_result_path, '1.png')
-                        cv2.imwrite(f1, questions_cols[0])
-                        f2 = join(train_result_path, '2.png')
-                        cv2.imwrite(f2, questions_cols[1])
-                        f3 = join(train_result_path, '3.png')
-                        cv2.imwrite(f3, questions_cols[2])
-                        '''
+                        for x, y, r in circles[0]:
+                            question = question_number(y, i)
+                            # if a question appeared more than once remove it's mark
+                            # (2 choices are picked for the same question)
+                            # print x, y, r
+                            if question not in answers:
+                                answers[question] = answer(x)
+                                if answers[question] == correct_answers[question]:
+                                    score += 1
+                            else:
+                                score -= 1
+                            cv2.circle(backtorgb, (int(x + 90 + 330*i), int(y) + 70), r, (0, 0, 255), -1)
+                # fig, ax = plt.subplots()
+                # im = ax.imshow(backtorgb[70:665, :], interpolation='none')
+                # plt.show()
+                # cv2.imshow('image',backtorgb)
+                # save the images
+                print answers
+                print score
+                result_filename = join(train_result_path, f)
+                cv2.imwrite(result_filename, backtorgb)
 
 cv2.waitKey(0)
